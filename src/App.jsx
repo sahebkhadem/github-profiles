f9bc6cec33a0577edf460d09dd0153b08c21e5f7
TakaoIsTheBest <saheb1379@gmail.com>
2022-12-05 12:10:34 +0330

    create navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import { FaGithub, FaSun } from "react-icons/fa";
+
 function App() {
-	return <div></div>;
+	return (
+		<div className="App">
+			<nav>
+				<a href="#" className="logo">
+					<FaGithub />
+					<span>GitHub Profiles</span>
+				</a>
+
+				<div className="nav-items">
+					<a href="https://github.com/TakaoIsDaBest/github-profiles" target={"_blank"}>
+						<FaGithub />
+						<span>Repo</span>
+					</a>
+
+					<button title="Switch to light theme">
+						<FaSun />
+					</button>
+				</div>
+			</nav>
+			<main></main>
+		</div>
+	);
 }
 
 export default App;
